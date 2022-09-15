5a1724e97119908d470b663ad2cd9fc3222932ab
Ariel Arias <110634735+ArielArias93@users.noreply.github.com>
2022-09-14 22:28:26 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import Cuadrado from './components/cuadrado';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Cuadrado></Cuadrado>
+    </div>
+  );
+}
+
+export default App;
