8f44f83da91f977feb757530180beb790ac760a3
Ariel Arias <110634735+ArielArias93@users.noreply.github.com>
2022-11-28 21:40:10 -0300

    Add files via upload

---
+++ Practico19,20y21/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CardContainer from './components/chistes';
+
+function App() {
+  return (
+    <div className="App">
+      
+      
+      <CardContainer></CardContainer>
+    </div>
+  );
+}
+
+export default App;
